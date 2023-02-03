bae4964499a8020230797726dda85634f65c0564
EdtheEgghead <edzeegger@gmail.com>
2023-02-02 16:08:48 -0800

    Creates StringServer

--- NumberServer.java
+++ NumberServer.java
@@ -4,21 +4,34 @@ import java.net.URI;
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    int num = 0;
+    String string = new String();
 
     public String handleRequest(URI url) {
+        // if (url.getPath().equals("/")) {
+        //     return String.format("Edgar's Number: %d", num);
+        // } else if (url.getPath().equals("/increment")) {
+        //     num += 1;
+        //     return String.format("Number incremented!");
+        // } else {
+        //     System.out.println("Path: " + url.getPath());
+        //     if (url.getPath().contains("/add")) {
+        //         String[] parameters = url.getQuery().split("=");
+        //         if (parameters[0].equals("count")) {
+        //             num += Integer.parseInt(parameters[1]);
+        //             return String.format("Number increased by %s! It's now %d", parameters[1], num);
+        //         }
+        //     }
+        //     return "404 Not Found!";
+        // }
         if (url.getPath().equals("/")) {
-            return String.format("Edgar's Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
+            return String.format("Edgar's Number: %s", string);
         } else {
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
+            //System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add-message")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                if (parameters[0].equals("s")) {
+                    string = string + parameters[1];
+                    return String.format(string);
                 }
             }
             return "404 Not Found!";
---
+++ StringServer.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    String string = new String();
+    int count = 0;
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format(string);
+        } else {
+            if (url.getPath().contains("/add-message")) {
+                String[] parameters = url.getQuery().split("=");
+                if(parameters[0].equals("s")) {
+                    if(count > 0){
+                        string = string + "\n" + parameters[1];
+                        count++;
+                        return String.format(string);
+                    }
+                    string = string + parameters[1];
+                    count++;
+                    return String.format(string);
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
